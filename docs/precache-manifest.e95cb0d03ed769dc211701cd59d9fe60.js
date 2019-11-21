e91c5ec61e187d182ce0d1afc59ae43b16703cfc
unknown <obrienules@gmail.com>
2019-11-21 04:25:55 +0100

    rename build folder to docs

---
+++ docs/precache-manifest.e95cb0d03ed769dc211701cd59d9fe60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed877c28b1da4c698b9d2336be61d111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02edbe4f3079164cc994",
+    "url": "/static/css/main.ca7d5c1e.chunk.css"
+  },
+  {
+    "revision": "39d11e1db7ef72a304e0",
+    "url": "/static/js/2.9186929c.chunk.js"
+  },
+  {
+    "revision": "02edbe4f3079164cc994",
+    "url": "/static/js/main.c7089ee1.chunk.js"
+  },
+  {
+    "revision": "c826cce734281d2aebce",
+    "url": "/static/js/runtime-main.743a21ce.js"
+  }
+]);
