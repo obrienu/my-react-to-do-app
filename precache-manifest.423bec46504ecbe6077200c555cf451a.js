3fcd65ba4b3bda428d1e3b1282f8ec7dc9f4829e
unknown <obrienules@gmail.com>
2019-11-29 01:23:11 +0100

    Updates

---
+++ precache-manifest.423bec46504ecbe6077200c555cf451a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66892a28ebf679b440cde7d76eda7391",
+    "url": "/my-react-todo-app/index.html"
+  },
+  {
+    "revision": "f8d4bc7e32ec4ec5dc4b",
+    "url": "/my-react-todo-app/static/css/main.ca7d5c1e.chunk.css"
+  },
+  {
+    "revision": "39d11e1db7ef72a304e0",
+    "url": "/my-react-todo-app/static/js/2.9186929c.chunk.js"
+  },
+  {
+    "revision": "f8d4bc7e32ec4ec5dc4b",
+    "url": "/my-react-todo-app/static/js/main.89963d29.chunk.js"
+  },
+  {
+    "revision": "2ff3040c7352be629d8a",
+    "url": "/my-react-todo-app/static/js/runtime-main.e261d1c3.js"
+  }
+]);
